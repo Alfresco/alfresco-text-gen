a156fd77722d8e2109c99a9c9b74a1f060c3176e
Derek Hulley <derekhulley@gmail.com>
2015-02-17 16:23:42 +0000

    Remove unused import

--- src/main/java/org/alfresco/textgen/TextGenerator.java
+++ src/main/java/org/alfresco/textgen/TextGenerator.java
@@ -22,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Locale;
 import java.util.Random;
 
 import org.springframework.core.io.ClassPathResource;
